50af52bcb315a4a3f5d0b0ae44be16796c94b40a
Ayça Nursena Korkmaz <aycanursenakorkmaz@gmail.com>
2023-03-05 20:01:39 +0300

    Add project files.

---
+++ C-SharpProjeleri/KolaySeviyeProjeler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpProjeleri
+{
+    internal class KolaySeviyeProjeler
+    {
+        public void TerstenYazdır()
+        {
+            Console.Write("Bir ifade giriniz : ");
+            string input = Console.ReadLine();
+
+            string[] kelime = input.Split(' ');
+
+            foreach(string s in kelime)
+            {
+                for (int i = 1; i < s.Length; i++)
+                {
+                    Console.Write(s[i]);
+                }
+                Console.Write(s[0] + " ");
+            }
+        }
+
+        public void Algoritma()
+        {
+            bool sayiMi = false;
+            int x = 0;
+            Console.Write("Bir kelime ve bir sayı giriniz (aralarında virgül ile) : ");
+            string input = Console.ReadLine();
+
+            string[] girilenDeger = input.Split(',');
+            List<int> sayilar = new List<int>();
+            List<string> kelimeler = new List<string>();
+
+            foreach (var item in girilenDeger)
+            {     
+                try
+                {
+                    int a = int.Parse(item);
+                    sayilar.Add(a);
+                    sayiMi = true;
+                }
+                catch (Exception)
+                {
+                    kelimeler.Add(item);
+                }
+            }
+
+            foreach (string kelime in kelimeler)
+            {
+                for (int i = 0; i < kelime.Length; i++)
+                {
+                    if (i == sayilar[x])
+                    {
+                        kelime.Remove(i);
+                        continue;
+                    }
+                    Console.Write(kelime[i]);
+                }
+                x++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C-SharpProjeleri/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace C_SharpProjeleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KolaySeviyeProjeler yeni = new KolaySeviyeProjeler();
+            //yeni.TerstenYazdır();
+            yeni.Algoritma();
+        } 
+    }
+}
