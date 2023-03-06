b836154f79d264f3ed08faf10f7388fc2b5d6e8b
Ayça Nursena Korkmaz <aycanursenakorkmaz@gmail.com>
2023-03-06 16:22:44 +0300

    mutlak kare alma eklendi

---
+++ MutlakKareAlma/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MutlakKareAlma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı giriniz: ");
+            string sayilar = Console.ReadLine();
+            string[] sayiListesi = sayilar.Split(' ');
+
+            int toplam = 0;
+            double mutlakToplam = 0;
+
+            foreach (var sayi in sayiListesi)
+            {
+                int x = int.Parse(sayi);
+               
+                if(x < 67)
+                {
+                    int y = 67 - x;
+                    toplam += y;
+                }
+                else
+                {
+                    double a = Math.Pow(x - 67, 2);   
+                    mutlakToplam += a;
+                }
+            }
+
+            Console.Write(toplam + " " + mutlakToplam);
+        }
+    }
+}
