7b43b08161f6396a0c5505217562373b4a209b51
Ayça Nursena Korkmaz <aycanursenakorkmaz@gmail.com>
2023-03-06 16:07:04 +0300

    ikili hesaplama eklendi

---
+++ IkiliHesaplama/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IkiliHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hesaplanacak sayıları giriniz : ");
+            string sayılar = Console.ReadLine();
+            string[] sayilar = sayılar.Split(' ');
+            
+            for(int i = 0; i < sayilar.Length; i+=2)
+            {
+                if (sayilar[i] != sayilar[i + 1])
+                {
+                    int toplam = int.Parse(sayilar[i]) + int.Parse(sayilar[i + 1]);
+                    Console.Write(toplam + " ");
+                }else
+                {
+                    int b = int.Parse(sayilar[i]);
+                    double sonuc = Math.Pow(b * b, 2);
+                    Console.Write(sonuc + " ");
+                }
+            }
+        }
+    }
+}
