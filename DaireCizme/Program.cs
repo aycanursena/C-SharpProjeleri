b276ae4584324b98a01d5b1e0151e90a5cb159bf
Ayça Nursena Korkmaz <aycanursenakorkmaz@gmail.com>
2023-03-06 23:18:45 +0300

    daire çizme

---
+++ DaireCizme/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DaireCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double yaricap;
+            double kalinlik = 0.4;
+
+            Console.Write("Yarıçapını giriniz: ");
+            if (!double.TryParse(Console.ReadLine(), out yaricap) || yaricap <= 0)
+            {
+                Console.WriteLine("Yarıçapta negatif değer girilemez !!");
+                return;
+            }
+
+            while (yaricap <= 0) ;
+
+            Console.WriteLine();
+            double rin = yaricap - kalinlik, rOut = yaricap + kalinlik;
+
+            for (double y = yaricap; y >= -yaricap; --y)
+            {
+                for (double x = -yaricap; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rin * rin && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UcgenCizme/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UcgenCizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz : ");
+            int inp = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < inp; i++)
+            {
+                for (int a = 0; a <= i; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
